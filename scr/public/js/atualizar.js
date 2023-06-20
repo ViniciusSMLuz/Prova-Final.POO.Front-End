031f760c1df144f721e4cf691cda263b6383e7b2
V125-dev <110831331+ViniciusSMLuz@users.noreply.github.com>
2023-06-20 20:11:10 -0300

    classe atualizar html/js

---
+++ scr/public/js/atualizar.js
@@ -0,0 +1,36 @@
+async function atualizarProduto() {
+    id = document.getElementById('input-idProduto').value;
+    let = produto = {
+
+       "idProduto": document.getElementById('input-idProduto').value,
+       "nomeProduto": document.getElementById('input-nomeProduto').value,
+       "marcaProduto": document.getElementById('input-marcaProduto').value,
+       "unidadeDeMedida": parseFloat(document.getElementById('input-unidadeDeMedida').value),
+       "quantidadeProduto": document.getElementById('input-quantidadeEmEstoque').value,
+       "descProduto": document.getElementById('input-descProduto').value
+    }
+
+    try {
+        const response = await fetch(`http://localhost:8080/estoque/atualizar-produto/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(produto)
+        });
+
+        if (response.ok) {
+            console.log('Produto atualizado com sucesso');
+            alert('Produto atualizado com sucesso');
+            listarProdutos();
+        } else {
+            console.error('Falha ao atualizar o Produto:', response.status, response.statusText);
+            alert('Falha ao atualizar o Produto:', response.status, response.statusText);
+        }
+    } catch (error) {
+        console.error('Erro na requisição de Update:', error);
+        alert('Erro na requisição de Update:', error);
+    }
+    listarProdutos();
+
+}
