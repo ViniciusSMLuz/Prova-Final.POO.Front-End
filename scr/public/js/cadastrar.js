2b5178bc2a13e91c0947bba44cf2cd5f1cab4dc9
EttoreENC <99734573+EttoreENC@users.noreply.github.com>
2023-06-20 16:26:09 -0300

    cadastrar.js inicial

---
+++ scr/public/js/cadastrar.js
@@ -0,0 +1,33 @@
+async function cadastrarLivro() {
+  var Livro = {
+      "id_produto": uuidv4(),
+      "nome_produto": document.getElementById('detail-nome_produto').value,
+      "marca_produto": document.getElementById('detail-marca_produto').value,
+      "unidade_de_medida": document.getElementById('detail-unidade_de_medida').value,
+      "quantidade_produto": document.getElementById('detail-quantidade_produto').value,
+      "desc_produto": document.getElementById('detail-desc_produto').value   
+  }
+  let envio = {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json"
+      },
+      body: JSON.stringify(Livro)
+  };
+  
+  try {
+     let response = await fetch("http://localhost:8080/estoque/produtos", envio);
+     listLivros();
+     if (response.ok) {
+          console.log('Produto cadastrado com sucesso');
+          alert('Produto cadastrado com sucesso');
+          listarProdutos();
+      } else {
+          console.error('Falha ao cadastrar produto:', response.status, response.statusText);
+          alert('Falha ao cadastrar produto:', response.status, response.statusText);
+      }
+  } catch (error) {
+      console.error('Erro na requisição do cadastro:', error);
+      alert('Erro na requisição do cadastro:', error);
+  }
+}
